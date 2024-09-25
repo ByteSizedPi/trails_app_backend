c64937944b1ee4d64e1b37887672974e986c9610
Johan Venter <johann12venter@gmail.com>
2024-09-25 19:01:13 +0200

    test run

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
 from app import app
 import os
+from app import app
+import os
 
 if __name__ == "__main__":
-    app.run(debug = not not os.getenv("DEBUG"), port=os.getenv("PORT", 5000))
+    env = ["DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "APP_ROOT_PASSWORD", "DEBUG"]
+    
+    with open("output.txt", "w") as file:
+        for var in env:
+            file.write(f"{var}: {os.getenv(var)}\n")
+    
+    app.run(debug=not not os.getenv("DEBUG"), port=os.getenv("PORT", 5000))
